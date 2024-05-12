c6f38b467cb2352690973dc848b989c10d13d3c4
waltersegovia <wseg@hotmail.com>
2024-05-12 01:51:02 -0300

    sala

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegistrarUsuarioComponent } from './components/registrar-usuario/registrar-usuario.component';
 import { VerificarCorreoComponent } from './components/verificar-correo/verificar-correo.component';
-import { RecuperarPasswordComponent } from './components/recuperar-password/recuperar-password.component';
+//import { RecuperarPasswordComponent } from './components/recuperar-password/recuperar-password.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 import { ReactiveFormsModule } from '@angular/forms';
@@ -13,7 +13,7 @@ export const routes: Routes = [
     {path:'login', component: LoginComponent},
     {path:'registrar-usuario', component: RegistrarUsuarioComponent},
     {path:'verificar-correo', component: VerificarCorreoComponent},
-    {path:'recuperar-password', component: RecuperarPasswordComponent},
+    //{path:'recuperar-password', component: RecuperarPasswordComponent},
     {path:'dashboard', component: DashboardComponent},
     {path:'home', component: HomeComponent},
     {path:'**', redirectTo: '/login', pathMatch: "full"}
